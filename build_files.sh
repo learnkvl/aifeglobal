5f5ba5abcabac11814efa28acd60333d21767050
KILUMU VENKATA LAXMI <kvlakshmi2358@gmail.com>
2025-06-05 13:45:30 +0530

    build

--- build_files.sh
+++ build_files.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-# Ensure pip is available
+# Python setup
 /usr/bin/python3.9 -m ensurepip --upgrade
-
-# Upgrade pip
 /usr/bin/python3.9 -m pip install --upgrade pip
-
-# Install dependencies from requirements.txt
 /usr/bin/python3.9 -m pip install -r requirements.txt
 
-# Create static directory if it doesn't exist
-mkdir -p /vercel/path0/static
-
-# Collect static files
+# Django setup
 python3.9 manage.py collectstatic --noinput
-
-# Apply database migrations
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
+
+# Create public directory and move static files into it
+mkdir -p public/static
+cp -r staticfiles/* public/static/
